e78ec0f999daca0e84a43d55f648cd590f959456
anpoliakov <it.anpoliakov@gmail.com>
2024-05-22 22:47:01 +0300

    add first commit

---
+++ src/test/java/by/anpoliakov/tiffbankeffectivemobile/TiffbankEffectiveMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package by.anpoliakov.tiffbankeffectivemobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiffbankEffectiveMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
